51d6dc81e2b9e003d0f0f7549077ac36a7067cf4
Shreya singh. <shreyaaa1314@gmail.com>
2025-01-30 14:00:31 +0530

    Revert "vghContact"

--- contacts/app.js
+++ contacts/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
-
 var app = express();
 
 // view engine setup
@@ -24,7 +23,6 @@ app.use('/', indexRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-  
 });
 
 // error handler
